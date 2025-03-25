22fb231d7d9588e29a0d89016e9c96dab9bf1847
thanhphong10997 <thanhphong0997@gmail.com>
2025-03-25 20:59:27 +0700

    Adding Missing Authentication and Authorization for all routes

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -4,20 +4,18 @@ const authController = require('../controllers/authController')
 
 const router = express.Router({ mergeParams: true }) //  pass the query params from the router
 
+// When user want to modify the data, it will need to pass this middleware to go through the code below and access router paths
+router.use(authController.protect)
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.setTourUserIds,
-    reviewController.createReview
-  )
+  .post(authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview)
 
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview)
+  .patch(authController.restrictTo('user', 'admin'), reviewController.updateReview)
+  .delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview)
 
 module.exports = router
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,15 +9,22 @@ router.post('/signup', authController.signUpUser)
 router.post('/signin', authController.signInUser)
 router.post('/forgot-password', authController.forgotPassword)
 router.patch('/reset-password/:token', authController.resetPassword)
-router.patch('/update-my-password', authController.protect, authController.updatePassword)
+
+// When user want to modify the data, it will need to pass this middleware to go through the code below and access router paths
+// instead of add protect to each of the routes such as (/update-me, /delete-me, /:id, etc...)
+router.use(authController.protect)
 
 // modify user data
-router.route('/me').get(authController.protect, userController.getMe, userController.getUser)
-router.patch('/update-me', authController.protect, userController.updateMe)
-router.delete('/delete-me', authController.protect, userController.deleteMe)
+router.patch('/update-my-password', authController.updatePassword)
+router.route('/me').get(userController.getMe, userController.getUser)
+router.patch('/update-me', userController.updateMe)
+router.delete('/delete-me', userController.deleteMe)
 
+// authorization with admin access
+router.use(authController.restrictTo('admin'))
+
+// Interact with user data
 router.route('/').get(userController.getAllUsers)
-
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser)
 
 module.exports = router
